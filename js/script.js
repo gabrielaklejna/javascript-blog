ee1af30c48718de2e1301ae8ccdb54d840a546d6
Gabriela Klejna <gabriela.rentel@gmail.com>
2022-09-29 18:27:30 +0200

    add .

--- js/script.js
+++ js/script.js
@@ -30,6 +30,7 @@
   }
   
   const links = document.querySelectorAll('.titles a');
+  console.log();
   
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
@@ -39,24 +40,57 @@
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
 
-  function generateTitleLinks(){
+function generateTitleLinks(){
 
   /* remove contents of titleList */
-     const postContent = document.querySelector(optTitleListSelector)
+  titleList = document.querySelector(optTitleListSelector).innerHTML = '';
+
   /* for each article */
-     for (let post of postContent){
-      postContent.classList.remove(optTitleListSelector)
-     }
+  const articles = titleList.querySelector('.post');
+   for (let article of articles){
+    article.titleList.remove(optTitleListSelector); 
     /* get the article id */
-    
+    const articleId = '.id';
     /* find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML; /* typo wyszukuje post-title i innerHTML moze go modykifowac  *
 
     /* get the title from the title element */
-
+    articleTitle.titleList.add(optTitleSelector);
     /* create HTML of the link */
-
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log();
     /* insert link into titleList */
-
+    html = html + linkHTML;
   }
+  titleList.innerHTML = html;
+}
 
-  generateTitleLinks();
+generateTitleLinks();
+
+function generateTags(){
+  /* find all articles */
+
+  /* START LOOP: for every article: */
+
+    /* find tags wrapper */
+
+    /* make html variable with empty string */
+
+    /* get tags from data-tags attribute */
+
+    /* split tags into array */
+
+    /* START LOOP: for each tag */
+
+      /* generate HTML of the link */
+
+      /* add generated code to html variable */
+
+    /* END LOOP: for each tag */
+
+    /* insert HTML of all the links into the tags wrapper */
+
+  /* END LOOP: for every article: */
+}
+
+generateTags();
